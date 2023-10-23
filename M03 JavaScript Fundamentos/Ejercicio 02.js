997bd3f449af10142cda8cd3c021f3b67d72cd65
Briannob23 <brianruiznob2023@gmail.com>
2023-10-22 23:57:47 -0300

    Esta dificil pero de a poco voy

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -3,15 +3,18 @@
 
 function devolverString(string) {
    // Debe retornar un string.
-   // Tu código:
+   // Tu código: 
+ return "Henry";
 }
-
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+    return x + y;
+    
 }
+
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
