51abe94c8cf8f203c78cbd36a774f5bc581d7fe3
Briannob23 <brianruiznob2023@gmail.com>
2023-10-23 00:04:48 -0300

    5 ejercicios modulo 03,Ej 02

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -19,16 +19,19 @@ function suma(x, y) {
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return x - y;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x / y;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return x * y;
 }
 
 function obtenerResto(x, y) {
